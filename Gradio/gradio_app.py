7aa9557814d744f5fc1c3523af322a629115d47d
AFlo59 <etudessup59230@gmail.com>
2024-06-28 14:35:11 +0200

    up

---
+++ Gradio/gradio_app.py
@@ -0,0 +1,64 @@
+import gradio as gr
+import cv2
+from ultralytics import YOLO
+import numpy as np
+from PIL import Image
+
+# Charger le modèle YOLOv8 fine-tuné
+model = YOLO('yolov8_fall_detection.pt')
+
+# Fonction de prédiction
+def predict():
+    cap = cv2.VideoCapture(0)  # 0 pour utiliser la webcam par défaut
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = model(frame_rgb)
+
+        status = "Nothing detected"
+        for result in results:
+            for box in result.boxes:
+                x1, y1, x2, y2 = map(int, box.xyxy[0])
+                class_id = int(box.cls[0])
+                confidence = box.conf[0]
+
+                # Vérifiez que la classe existe dans les noms
+                if class_id in result.names:
+                    label = result.names[class_id]
+
+                    if label.lower() == 'fall':  # Assurez-vous que le label pour la chute est 'fall'
+                        color = (0, 0, 255)  # Rouge pour les chutes
+                        text = f"Fall detected: {confidence:.2f}"
+                        status = "Fall detected"
+                        # Dessiner le rectangle autour de l'objet détecté
+                        cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
+                        cv2.putText(frame, text, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, color, 2)
+                    else:
+                        color = (0, 255, 0)  # Vert pour d'autres objets
+                        text = f"{label}: {confidence:.2f}"
+                        # Dessiner le rectangle autour de l'objet détecté
+                        cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
+                        cv2.putText(frame, text, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, color, 2)
+
+        frame_pil = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+        yield frame_pil, status
+
+    cap.release()
+
+# Créer l'interface Gradio
+iface = gr.Interface(
+    fn=predict,
+    inputs=None,
+    outputs=[gr.Image(type="pil", label="Detection"), gr.Textbox(label="Status")],
+    live=True,
+    title="Detection de Chutes en Temps Réel",
+    description="Utilisez votre webcam pour détecter les chutes en temps réel."
+)
+
+# Lancer l'interface
+if __name__ == "__main__":
+    iface.launch()
